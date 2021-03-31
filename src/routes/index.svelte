acc452d86679ed6144f7e308cf999e5f931aa1a3
CleitonLuizOliveira <cleitoneldorn@gmail.com>
2021-03-31 11:09:32 -0300

    inciciei o site

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<script>
+	
+</script>
+
+<style>
+
+</style>
+
+
+<title>
+	Rebeldes de Arkanna
+</title>
+
+
+<main>
+	<h1>Rebeldes de Arkanna</h1>
+
+	<div class="session">
+		<h2>Sessão 1</h2>
+		<p>
+			O grupo começa a aventura na taverna "O Unicórnio Voador", Vanessa acaba de acordar de um pesadelo recorrente, todos se reunem ali. Logo, Cora Bombarril, a ladina, chega com a notícia que as tropas saíram apra a floresta, para derrotar uma criatura parecida com um observador.
+			Eles partem para a fazendo habitada pelo casal de anões Ingo e Helga von Shtronk. após uma conversa entre o bardo, a clériga, o guardião e os dois anões, o grupo ganhaa chave para o posso da propriedade, onde o suposto tesouro estaria escondido.
+			Já na entrada do poço o grupo pode ouvir roncos assustadores, uqe lgoo descobrem que vem de um casal de Bicos de machado e seus dois filhotes. Sendo que a Bico de Machado femea estava com uma chave ao redor do seu pescoço. Cora tenta remover a chave sem acordar o animal, mas falha, acordando todos eles. Vanessa, porém, consegue acalmar os animos dos passáros, permitindo que o grupo saia da sala com a chave em mãos.
+			O posso tem 4 salas principais, em cada uma delas está uma estua da deusa Malvys.
+		</p>
+	</div>
+
+	
+</main>
